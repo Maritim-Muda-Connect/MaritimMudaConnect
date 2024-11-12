f641d6d825c116e35c841483b6b10e1afbf89e14
Genthur <genthuraryoseto510@gmail.com>
2024-11-12 14:20:42 +0700

    fixing bug when all fields are empty

--- lib/app/modules/widget/custom_dropdown.dart
+++ lib/app/modules/widget/custom_dropdown.dart
@@ -19,64 +19,58 @@ class CustomDropdown extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 48,
-      width: double.infinity,
-      decoration: BoxDecoration(
-        color: neutral02Color,
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: DropdownButtonFormField<String>(
-        validator: validator,
-        value: selectedOption != '' && selectedOption != hintText ? selectedOption : null,
-        onChanged: onSelected,
-        items: options.map((String value) {
-          return DropdownMenuItem<String>(
-            value: value,
-            enabled: value != hintText,
-            child: Text(
-              value,
-              style: regulerText12.copyWith(color: hintText == value ? neutral03Color : neutral04Color),
-            ),
-          );
-        }).toList(),
-        hint: hintText != null
-            ? Text(
-          hintText!,
-          style: regulerText12.copyWith(color: neutral03Color),
-        )
-            : null,
-        decoration: InputDecoration(
-          contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-            borderSide: BorderSide(color: neutral03Color, width: 1),
+    return DropdownButtonFormField<String>(
+      validator: validator,
+      value: selectedOption != '' && selectedOption != hintText ? selectedOption : null,
+      onChanged: onSelected,
+      items: options.map((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          enabled: value != hintText,
+          child: Text(
+            value,
+            style: regulerText12.copyWith(color: hintText == value ? neutral03Color : neutral04Color),
           ),
-          enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-            borderSide: BorderSide(color: neutral04Color, width: 1),
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-            borderSide: BorderSide(color: neutral04Color, width: 1),
-          ),
-        ),
+        );
+      }).toList(),
+      hint: hintText != null
+          ? Text(
+        hintText!,
         style: regulerText12.copyWith(color: neutral03Color),
-        dropdownColor: neutral02Color,
-        icon: Icon(Icons.arrow_drop_down, color: neutral03Color),
-        isExpanded: true,
-        menuMaxHeight: 200,
-        selectedItemBuilder: (BuildContext context) {
-          return options.map<Widget>((String item) {
-            return Text(
-              item,
-              style: regulerText12.copyWith(color: neutral04Color),
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis,
-            );
-          }).toList();
-        },
+      )
+          : null,
+      decoration: InputDecoration(
+        contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide(color: neutral03Color, width: 1),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide(color: neutral04Color, width: 1),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide(color: neutral04Color, width: 1),
+        ),
+        filled: true,  // Tambahkan ini
+        fillColor: neutral02Color,
       ),
+      style: regulerText12.copyWith(color: neutral03Color),
+      dropdownColor: neutral02Color,
+      icon: Icon(Icons.arrow_drop_down, color: neutral03Color),
+      isExpanded: true,
+      menuMaxHeight: 200,
+      selectedItemBuilder: (BuildContext context) {
+        return options.map<Widget>((String item) {
+          return Text(
+            item,
+            style: regulerText12.copyWith(color: neutral04Color),
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+          );
+        }).toList();
+      },
     );
   }
 }
